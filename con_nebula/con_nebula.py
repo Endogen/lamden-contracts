f3372dcf6d82d0de6e436d20553eb34652c2c45f
endogen <081081@gmx.net>
2021-08-25 23:28:45 +0200

    Changed NEB tax and total / circ. supply

--- con_nebula/con_nebula.py
+++ con_nebula/con_nebula.py
@@ -6,11 +6,13 @@ metadata = Hash()
 tax_percent = Variable()
 swap_allowed = Variable()
 vault_contract = Variable()
-tax_whitelist = Variable()
+tax_blacklist = Variable()
+total_supply = Variable()
 
 SWAP_FACTOR = 0.01
-BURN_ADDRESS = 'OH_SHIT_ITS_GONE'
-SWAP_END_DATE = now + datetime.timedelta(days=2)
+BURN_ADDRESS = 'NEBULA_BURN_ADDRESS'
+INTERNAL_VAULT = 'internal_neb_vault'
+SWAP_END_DATE = now + datetime.timedelta(days=1)
 OPERATORS = [
     'ae7d14d6d9b8443f881ba6244727b69b681010e782d4fe482dbfb0b6aca02d5d',
     'e787ed5907742fa8d50b3ca2701ab8e03ec749ced806a15cdab800a127d7f863'
@@ -24,10 +26,11 @@ def seed():
     metadata['token_symbol'] = "TNEB"
     metadata['operator'] = ctx.caller
 
-    tax_percent.set(10)
+    tax_percent.set(1)
     swap_allowed.set(False)
-    tax_whitelist.set(['con_amm_v9'])
-    vault_contract.set('con_test_nebula_vault')
+    tax_blacklist.set([])
+    vault_contract.set('')
+    total_supply.set(0)
 
 @export
 def change_metadata(key: str, value: Any):
@@ -41,11 +44,10 @@ def transfer(amount: float, to: str):
     assert balances[ctx.caller] >= amount, 'Not enough coins to send!'
 
     balances[ctx.caller] -= amount
+    balances[to] += amount
 
-    if to in tax_whitelist.get():
-        balances[to] += amount
-    else:
-        balances[to] += pay_tax_return_rest(amount)
+    if to in tax_blacklist.get():
+        pay_tax(amount)
 
 @export
 def approve(amount: float, to: str):
@@ -61,60 +63,69 @@ def transfer_from(amount: float, to: str, main_account: str):
 
     balances[main_account, ctx.caller] -= amount
     balances[main_account] -= amount
+    balances[to] += amount
 
-    if to in tax_whitelist.get():
-        balances[to] += amount
-    else:
-        balances[to] += pay_tax_return_rest(amount)
+    if to in tax_blacklist.get():
+        pay_tax(amount)
 
-# ------ TAX & WHITELIST ------
+# ------ TAX ------
 
-def pay_tax_return_rest(amount: float):
-    tax = int(amount / 100 * tax_percent.get())
+def pay_tax(amount: float):
+    tax_amount = int(amount / 100 * tax_percent.get())
 
-    if tax > 0:
-        balances[vault_contract.get()] += tax
-        return amount - tax
-    else:
-        return amount
+    if tax_amount > 0:
+        difference = int(balances[ctx.signer] - tax_amount)
+        assert balances[ctx.signer] >= tax_amount, 'Not enough coins to pay for NEB tax. Missing {} NEB'.format((difference * -1) + 1)
 
-@export
-def set_transaction_tax(tax: float):
-    assert_owner()
-    assert (tax_percent.get() >= 0 and tax_percent.get() <= 100), 'Value must be between 0 and 100'
+        if not vault_contract.get():
+            vault = INTERNAL_VAULT
+        else:
+            vault = vault_contract.get()
 
-    tax_percent.set(tax)
+        balances[vault] += tax_amount
+        balances[ctx.signer] -= tax_amount
 
 @export
-def add_to_tax_whitelist(contract: str):
+def set_tax(tax_in_percent: float):
     assert_owner()
-    assert contract not in tax_whitelist.get(), 'Contract already in tax whitelist!'
+    assert (tax_in_percent >= 0 and tax_in_percent <= 100), 'Value must be between 0 and 100'
 
-    lst = tax_whitelist.get()
-    lst.append(contract)
-    tax_whitelist.set(lst)
+    tax_percent.set(tax_in_percent)
 
 @export
-def remove_from_tax_whitelist(contract: str):
+def add_to_tax_blacklist(recipient: str):
     assert_owner()
-    assert contract in tax_whitelist.get(), 'Contract not in tax whitelist!'
+    assert recipient not in tax_blacklist.get(), 'Recipient already on tax blacklist'
 
-    lst = tax_whitelist.get()
-    lst.remove(contract)
-    tax_whitelist.set(lst)
+    lst = tax_blacklist.get()
+    lst.append(recipient)
+    tax_blacklist.set(lst)
+
+@export
+def remove_from_tax_blacklist(recipient: str):
+    assert_owner()
+    assert recipient in tax_blacklist.get(), 'Recipient not on tax blacklist'
+
+    lst = tax_blacklist.get()
+    lst.remove(recipient)
+    tax_blacklist.set(lst)
 
 # ------ SWAP ------
 
 @export
 def swap_gold(amount: float):
-    assert now < SWAP_END_DATE
-    assert swap_allowed.get() == True, 'Swapping GOLD for NEB is disabled'
+    assert now < SWAP_END_DATE, 'Swap period ended'
+    assert swap_allowed.get() == True, 'Swapping GOLD for NEB currently disabled'
     assert amount > 0, 'Cannot swap negative balances!'
 
     gold = I.import_module('con_test_gold_contract')
-    #gold.approve(amount=amount, to=BURN_ADDRESS)
     gold.transfer_from(amount=amount, to=BURN_ADDRESS, main_account=ctx.caller)
-    balances[ctx.caller] += amount * SWAP_FACTOR
+
+    swap_amount = amount * SWAP_FACTOR
+
+    balances[ctx.caller] += swap_amount
+
+    total_supply.set(total_supply.get() + swap_amount)
 
 @export
 def enable_swap():
@@ -129,29 +140,37 @@ def disable_swap():
 # ------ BURNING ------
 
 @export
-def burn_neb(amount: float):
+def burn(amount: float):
     assert amount > 0, 'Cannot burn negative amount!'
     assert balances[ctx.caller] >= amount, 'Not enough coins to burn!'
 
-    balances[ctx.caller] -= amount
     balances[BURN_ADDRESS] += amount
+    balances[ctx.caller] -= amount
 
 # ------ VAULT ------
 
 @export
-def set_vault_contract(contract: str):
+def set_vault(contract: str):
     assert_owner()
     vault_contract.set(contract)
+
+@export
+def flush_internal_vault():
+    assert_owner()
+    assert vault_contract.get(), 'Vault contract not set!'
+
+    balances[vault_contract.get()] += balances[INTERNAL_VAULT]
+    balances[INTERNAL_VAULT] = 0
 
 # ------ SUPPLY ------
 
 @export
 def circulating_supply():
-    return int(sum(balances.all()) - balances[BURN_ADDRESS])
+    return int(total_supply.get() - balances[BURN_ADDRESS])
 
 @export
 def total_supply():
-    return int(sum(balances.all()))
+    return int(total_supply.get())
 
 # ------ INTERNAL ------
 
