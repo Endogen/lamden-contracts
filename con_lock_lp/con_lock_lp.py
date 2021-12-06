c8f41f21b01bd2f55f100494f552de5bf12e9c53
endogen <081081@gmx.net>
2021-12-07 00:05:37 +0100

    Includes NEB fees and NEB BASE contract

--- con_lock_lp/con_lock_lp.py
+++ con_lock_lp/con_lock_lp.py
@@ -1,88 +1,141 @@
-import con_amm_v9 as rswp
+I = importlib
 
-lock_info = Hash(default_value="")
+lock_data = Hash(default_value="")
 
+dex = Variable()
+neb_base = Variable()
+lock_fee = Variable()
+max_lock_time = Variable()
+
+OPERATORS = [
+    'ae7d14d6d9b8443f881ba6244727b69b681010e782d4fe482dbfb0b6aca02d5d',
+    'e787ed5907742fa8d50b3ca2701ab8e03ec749ced806a15cdab800a127d7f863',
+    '62783e94c14b0ae0c555f33ca6aa9699099606d636d4f0fd916bf912d8022045'
+]
+
+# DONE
+@construct
+def seed():
+    lock_fee.set(1)
+    max_lock_time.set(365)
+    neb_base.set('con_neb_base_001')
+    dex.set('con_rocketswap_official_v1_1')
+
+# DONE
 @export
-def lock_lp(contract: str, amount: float, lock_time_in_days: int):
-    assert amount > 0, "Negative amount not allowed!"
+def lock_lp(lock_id: str, token_contract: str, lp_amount: float, lock_time_in_days: int):
+    assert lp_amount > 0, "Negative LP amount not allowed!"
     assert lock_time_in_days > 0, "Negative lock time not allowed!"
+    assert lock_time_in_days <= max_lock_time.get(), f"Max lock time is {max_lock_time} days"
+    assert lock_id not in lock_data, 'Lock ID already exists!'
 
-    lock_id = hashlib.sha256(f"{ctx.caller}{now}")
-    
-    rswp.transfer_liquidity_from(
-        contract=contract, 
+    neb_fee = lp_amount / 100 * lock_fee.get()
+
+    I.import_module(dex.get()).transfer_liquidity_from(
+        contract=token_contract, 
         to=ctx.this, 
         main_account=ctx.caller, 
-        amount=amount)
+        amount=lp_amount)
 
-    lock_info[lock_id] = {
-        "owner": ctx.caller, 
+    contracts = ForeignHash(foreign_contract=neb_base.get(), foreign_name='contracts')
+
+    I.import_module(dex.get()).transfer_liquidity(
+        contract=token_contract, 
+        to=contracts['treasury'], 
+        amount=neb_fee)
+
+    lock_data[lock_id] = {
         "start_date": now, 
-        "contract": contract, 
-        "amount": amount, 
-        "days": lock_time_in_days
+        "contract": token_contract, 
+        "returned": False, 
+        "amount": lp_amount - neb_fee, 
+        "owner": ctx.caller, 
+        "days": lock_time_in_days, 
+        "dex": dex.get(), 
     }
     
-    return f"Lock ID: {lock_id}"
- 
+    return f'{lock_data[lock_id]["start_date"] + datetime.timedelta(days=lock_data[lock_id]["days"])}'
+
+# DONE
 @export
 def unlock_lp(lock_id: str):
-    assert lock_info[lock_id] != "", "No LP lock for this ID!"
+    assert lock_id in lock_data, "Unknown lock ID!"
 
-    lock = lock_info[lock_id]
+    lock = lock_data[lock_id]
 
     assert lock["owner"] == ctx.caller, "You are not the owner!"
+    assert lock["returned"] == False, "Locked LP already returned!"
 
     lock_end = lock["start_date"] + datetime.timedelta(days=lock["days"])
 
-    assert now >= lock_end , f"LP lock date not reached: {lock_end}"
+    assert now >= lock_end , f"Unlock date not reached: {lock_end}"
 
-    rswp.transfer_liquidity(
+    I.import_module(lock["dex"]).transfer_liquidity(
         contract=lock["contract"], 
-        to=ctx.caller, 
+        to=lock["owner"], 
         amount=lock["amount"])
 
+    lock["returned"] = True
+    lock_data[lock_id] = lock
+
     return lock
-    
+
+# DONE
 @export
 def extend_time(lock_id: str, days_to_extend: int):
-    assert days_to_extend > 0, "Negative time not allowed!"
-    assert lock_info[lock_id] != "", "No LP lock for this ID!"
+    assert days_to_extend > 0, "Negative time extension not allowed!"
+    assert lock_id in lock_data, "Unknown lock ID!"
 
-    lock = lock_info[lock_id]
+    lock = lock_data[lock_id]
 
     assert lock["owner"] == ctx.caller, "You are not the owner!"
+    assert lock["returned"] == False, "Locked LP already returned!"
 
     lock["days"] += days_to_extend
-    lock_info[lock_id] = lock
 
-    return f"Lock time extended by {days_to_extend} days"
+    error = f'Max lock time is {max_lock_time} days. You try to set {lock["days"]} days'
+    assert lock["days"] <= max_lock_time.get(), error
 
+    lock_data[lock_id] = lock
+
+    return f'{lock["start_date"] + datetime.timedelta(days=lock["days"])}'
+
+# DONE
 @export
-def extend_amount(lock_id: str, amount: float):
-    assert amount > 0, "Negative amount not allowed!"
-    assert lock_info[lock_id] != "", "No LP lock for this ID!"
+def extend_amount(lock_id: str, lp_amount: float):
+    assert lp_amount > 0, "Negative LP amount not allowed!"
+    assert lock_id in lock_data, "Unknown lock ID!"
 
-    lock = lock_info[lock_id]
+    lock = lock_data[lock_id]
 
     assert lock["owner"] == ctx.caller, "You are not the owner!"
+    assert lock["returned"] == False, "Locked LP already returned!"
 
-    rswp.transfer_liquidity_from(
-        contract=lock["contract"], 
+    neb_fee = lp_amount / 100 * lock_fee.get()
+
+    I.import_module(dex.get()).transfer_liquidity_from(
+        contract=token_contract, 
         to=ctx.this, 
         main_account=ctx.caller, 
-        amount=amount)
+        amount=lp_amount)
 
-    lock["amount"] += amount
-    lock_info[lock_id] = lock
+    contracts = ForeignHash(foreign_contract=neb_base.get(), foreign_name='contracts')
 
-    return f"Lock amount extended by {amount}"
+    I.import_module(dex.get()).transfer_liquidity(
+        contract=token_contract, 
+        to=contracts['treasury'], 
+        amount=neb_fee)
 
+    lock["amount"] += lp_amount - neb_fee
+    lock_data[lock_id] = lock
 
+    return lock
+
+# DONE
 @export
 def time_until_unlock(lock_id: str):
-    assert lock_info[lock_id] != "", "No LP lock for this ID!"
-    return now - lock_info[lock_id]["start_date"]
+    assert lock_id in lock_data, "No LP locked for this ID!"
+    return f'{now - lock_data[lock_id]["start_date"]}'
 
 @export 
 def burn_lp(contract: str, amount: float):
@@ -94,9 +147,9 @@ def burn_lp(contract: str, amount: float):
 
 @export 
 def burn_locked_lp(lock_id: str):
-    assert lock_info[lock_id] != "", "No LP lock for this ID!"
+    assert lock_data[lock_id] != "", "No LP lock for this ID!"
 
-    lock = lock_info[lock_id]
+    lock = lock_data[lock_id]
 
     assert lock["owner"] == ctx.caller, "You are not the owner!"
 
@@ -105,3 +158,20 @@ def burn_locked_lp(lock_id: str):
         to="BURNED_LP", 
         main_account=ctx.this, 
         amount=lock["amount"])
+
+def set_neb_base(neb_base_contract: str):
+    neb_base.set(neb_base_contract)
+    assert_owner()
+
+def set_lock_fee(percent_of_lp: float):
+    assert percent_of_lp >= 0, "Fee can not be negative!"
+    lock_fee.set(percent_of_lp)
+    assert_owner()
+
+def set_max_lock_time(max_time_in_days: float):
+    assert max_time_in_days > =, "Max lock time can not be smaller than 1 day!"
+    max_lock_time.set(max_time_in_days)
+    assert_owner()
+
+def assert_owner():
+    assert ctx.caller in OPERATORS, 'Only executable by operators!'
