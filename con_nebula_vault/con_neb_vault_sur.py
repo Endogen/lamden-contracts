a9dcf5d61ce64f4cc5d326313a4414be9d3863e1
endogen <081081@gmx.net>
2022-02-03 02:27:51 +0100

    Added variable 'early unstake tax' & variable pre-funding token contract

--- con_nebula_vault/con_neb_vault_sur.py
+++ con_nebula_vault/con_neb_vault_sur.py
@@ -17,12 +17,15 @@ end_date = Variable()
 
 total_stake = Variable()
 total_emission = Variable()
+
+funding_contract = Variable()
+funding_amount = Variable()
+
 stake_contract = Variable()
+unstake_tax = Variable()
 active = Variable()
 
 NEBULA_FEE = 5
-EARLY_UNSTAKE_TAX = 5
-
 OPERATORS = [
     'ae7d14d6d9b8443f881ba6244727b69b681010e782d4fe482dbfb0b6aca02d5d',
     'e787ed5907742fa8d50b3ca2701ab8e03ec749ced806a15cdab800a127d7f863',
@@ -30,22 +33,35 @@ OPERATORS = [
 ]
 
 @export
-def init(minutes_till_start: int, start_period_minutes: int, minutes_till_end: int, token_contract: str, seed_amount: float = 0):
-    stake_contract.set(token_contract)
-    
-    total_emission.set(0)
+def init(minutes_till_start: int, start_period_minutes: int, minutes_till_end: int, stake_token_contract: str, 
+         pre_funding_token_contract: str = '', pre_funding_token_amount: float = 0, early_unstake_tax: float = 5):
+
+    if pre_funding_token_contract:
+        assert pre_funding_token_amount > 0, 'Pre-funding token amount must be > 0'
+    if pre_funding_token_amount:
+        assert pre_funding_token_amount > 0, 'Pre-funding token amount must be > 0'
+        assert pre_funding_token_contract, 'Pre-funding token contract must be set'
+
+    if stake_token_contract.lower() in ['currency', 'con_nebula']:
+        assert_owner('This staking token can only be used by the Nebula team')
+
     total_stake.set(0)
+    total_emission.set(0)
+
+    funding_contract.set(pre_funding_token_contract)
+    funding_amount.set(pre_funding_token_amount)
+
+    stake_contract.set(stake_token_contract)
+    unstake_tax.set(early_unstake_tax)
 
     start_date.set(now + datetime.timedelta(minutes=minutes_till_start))
     start_end_date.set(start_date.get() + datetime.timedelta(minutes=start_period_minutes))
     end_date.set(start_end_date.get() + datetime.timedelta(minutes=minutes_till_end))
 
-    if seed_amount > 0:
-        total_emission.set(seed_amount)
-
-        I.import_module(stake_contract.get()).transfer_from(
+    if funding_contract.get() and funding_amount.get():
+        I.import_module(funding_contract.get()).transfer_from(
+            amount=funding_amount.get(),
             main_account=ctx.caller,
-            amount=seed_amount,
             to=ctx.this)
 
     active.set(True)
@@ -77,20 +93,29 @@ def unstake():
 
     assert staking[ctx.caller] > 0, f'No stake found for this address'
 
-    # You survived! Pay out stake + emissions
+    # You survived! Pay out stake + emissions (+ pre funding if set)
     if now > end_date.get():
         stake_percent = staking[ctx.caller] / total_stake.get() * 100
-        user_emission = total_emission.get() / 100 * stake_percent
+        stake_emission = total_emission.get() / 100 * stake_percent
 
-        payouts[ctx.caller] = staking[ctx.caller] + user_emission
+        payouts[ctx.caller, stake_contract.get()] = staking[ctx.caller] + stake_emission
 
         I.import_module(stake_contract.get()).transfer(
             amount=payouts[ctx.caller],
             to=ctx.caller)
 
+        if funding_amount.get():
+            funding_emission = funding_amount.get() / 100 * stake_percent
+
+            payouts[ctx.caller, funding_contract.get()] += funding_emission
+
+            I.import_module(funding_contract.get()).transfer(
+                amount=funding_emission,
+                to=ctx.caller)
+
     # No diamond hands? Pay out stake - early unstake tax
     else:
-        tax = staking[ctx.caller] / 100 * EARLY_UNSTAKE_TAX
+        tax = staking[ctx.caller] / 100 * unstake_tax.get()
         total_emission.set(total_emission.get() + tax)
 
         payouts[ctx.caller] = staking[ctx.caller] - tax
@@ -101,7 +126,11 @@ def unstake():
             to=ctx.caller)
 
     staking[ctx.caller] = 0
-    return f'{payouts[ctx.caller]}'
+
+    funding_payout = payouts[ctx.caller, funding_contract.get()]
+    stake_payout = payouts[ctx.caller, stake_contract.get()]
+
+    return f'{funding_payout} {stake_payout}'
 
 @export
 def emergency_withdraw(contract: str, amount: float):
@@ -116,5 +145,6 @@ def emergency_set_stake(address: str, amount: float):
 def assert_active():
     assert active.get() == True, 'Vault inactive!'
 
-def assert_owner():
-    assert ctx.caller in OPERATORS, 'Only executable by operators!'
+def assert_owner(msg: str = ''):
+    error_msg = msg if msg else 'Only executable by operators!'
+    assert ctx.caller in OPERATORS, error_msg
